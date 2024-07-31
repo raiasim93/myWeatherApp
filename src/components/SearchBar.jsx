600201161ae4b53214672bcda1e878fbf2d4ac7e
raiasim93 <asimrai89@gmail.com>
2024-07-31 15:31:46 +1000

    style search bar, set root and import icon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,14 @@
-
-function SearchBar(){
-    return(
-      <header></header>  
-    )
+import React from 'react';
+import './SearchBar.css'
+import ThermostatRoundedIcon from '@mui/icons-material/ThermostatRounded';
+function SearchBar() {
+  return (
+  <div class="input-group mt-1 ">
+    < ThermostatRoundedIcon  />
+    <input type="search" class="form-control " placeholder="Find your city" aria-label="Search" aria-describedby="search-addon" />
+    <button type="button" class="search-bar-button btn-dark rounded" data-mdb-ripple-init>Search</button>
+</div>
+  );
 }
 
 export default SearchBar;
